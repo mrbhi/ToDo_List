ba0b5f12fac467b1f217538a5aa7d9abfd574aae
mrbhi <taofeeqworld@hotmail.com>
2023-09-23 08:23:06 -0700

    Jquery functionality added to other buttons

--- script/addlist.js
+++ script/addlist.js
@@ -1,135 +1,153 @@
 $(function() {
-    todoInput = document.getElementById('todo-input')
-addBtn = document.getElementById('btn-add')
-updateBtn = document.getElementById('btn-update')
-removeBtn = document.getElementById('btn-remove')
-list = $('#todo-list')
+    todoInput = $('#todo-input')
+    updateBtn = document.getElementById('btn-update')
+    removeBtn = $('#btn-remove')
+    list = $('#todo-list')
 
-currentTodoInput = ''
+    currentTodoInput = ''
 
-todoInput.addEventListener('input', function(e){
-    currentTodoInput = e.target.value
-})
-
-function createNewNode() {
-    newListElement = document.createElement('li')
-    buttonDelete = document.createElement('span')
-    textNode = document.createTextNode(currentTodoInput)
-    newListElement.id = ('item' + (list.childElementCount + 1))
-    buttonDelete.innerHTML = "Delete"
-    newListElement.append(textNode, buttonDelete)
-
-    buttonDelete.addEventListener('click', function() {
-        item = this.parentNode
-        item.remove()
+    todoInput.on({
+        'input': function(e){
+            currentTodoInput = e.target.value
+        },
+        'keyup': function(e){
+            if(e.keyCode === 13) {
+                createTODOItemAtBackend()
+            }
+        }
     })
 
-    return newListElement
-}
+    function createNewNode() {
+        newListElement = document.createElement('li')
+        buttonDelete = document.createElement('span')
+        textNode = document.createTextNode(currentTodoInput)
+        newListElement.id = ('item' + (list.childElementCount + 1))
+        buttonDelete.innerHTML = "Delete"
+        newListElement.append(textNode, buttonDelete)
 
-function addListItem() {
-    if (currentTodoInput !== '') {
+        buttonDelete.addEventListener('click', function() {
+            item = this.parentNode
+            item.remove()
+        })
 
-        newListElement = createNewNode()
-        
-        list.append(newListElement)
-    
-        todoInput.value = ''
+        return newListElement
+    }
+
+    function clearInputData() {
+        todoInput.val('')
         currentTodoInput = ''
-    } else {
-        alert('Enter a todo list')
-    }    
-    // firstListItem = list.firstElementChild
-    // list.insertBefore(newListElement, firstListItem)
-}
-
-todoInput.addEventListener('keyup', function(e){
-    if(e.keyCode === 13) {
-        createTODOItemAtBackend()
     }
-})
 
-addBtn.addEventListener("click", createTODOItemAtBackend)
 
-updateBtn.addEventListener("click", function() {
-    firstElement = list.firstElementChild
-    if (firstElement === null) {
-        alert('No item to be updated')
-    }else if(currentTodoInput !== '') {
-        newListElement = createNewNode()
-        list.replaceChild(newListElement, firstElement)
-    } else {
-        alert('Enter an item to update')
-    }
-})
 
-removeBtn.addEventListener('click', function() {
-    if (list.firstElementChild === null) {
-        alert("there's no item to be deleted")
-    } else {
-        list.remove()
-    }
-    
-})
+    $('#btn-add').click(createTODOItemAtBackend)
 
-function createTODODynamically(id, title) {
-    newListElement = document.createElement('li')
-    buttonDelete = document.createElement('span')
-    textNode = document.createTextNode(title)
-    newListElement.id = ('item' + id)
-    buttonDelete.innerHTML = "Delete"
-    newListElement.append(textNode, buttonDelete)
-
-    buttonDelete.addEventListener('click', function() {
-        item = this.parentNode
-        item.remove()
+    updateBtn.addEventListener("click", function() {
+        firstElement = list.firstElementChild
+        if (firstElement === null) {
+            alert('No item to be updated')
+        }else if(currentTodoInput !== '') {
+            newListElement = createNewNode()
+            list.replaceChild(newListElement, firstElement)
+        } else {
+            alert('Enter an item to update')
+        }
     })
 
-    return newListElement
-}
-
-function getTODOListFromBackend() {
-    const http = new XMLHttpRequest()
-    http.onreadystatechange = function() {
-        if(this.readyState === 4 ){
-            if(this.status === 200) {
-                const response = JSON.parse(this.responseText)
-                for(i=0; i < response.length; i++) {
-                    list.append(createTODODynamically(response[i].id, response[i].title))
-                }
-            } else {
-                console.log("Called Failed")
-            }
-            
-        }
-    }
-    http.open('GET', 'https://jsonplaceholder.typicode.com/todos', true)
-    http.send()
-}
-
-getTODOListFromBackend()
-
-
-function createTODOItemAtBackend() {
-    const http = new XMLHttpRequest()
-    http.open('POST', 'https://jsonplaceholder.typicode.com/todos', true)
-    http.onreadystatechange = function(){
-        if(this.readyState === 4) {
-            if(this.status === 201) {
-                const response = JSON.parse(this.responseText)
-                list.append(createTODODynamically(response.id, currentTodoInput))
-            } else {
-                console.log('Submission Failed')
-            }
+    removeBtn.click(function() {
+        if (list.firstElementChild === null) {
+            alert("there's no item to be deleted")
+        } else {
+            list.remove()
         }
         
-    }
-    const obj = JSON.stringify({
-        "userId": 1,
-        "title": currentTodoInput,
-        "completed": false
     })
-    http.send(obj)
-}
+    // removeBtn.addEventListener('click', function() {
+    //     if (list.firstElementChild === null) {
+    //         alert("there's no item to be deleted")
+    //     } else {
+    //         list.remove()
+    //     }
+        
+    // })
+
+    function createTODODynamically(id, title) {
+        newListElement = document.createElement('li')
+        buttonDelete = document.createElement('span')
+        textNode = document.createTextNode(title)
+        newListElement.id = ('item' + id)
+        buttonDelete.innerHTML = "Delete"
+        newListElement.append(textNode, buttonDelete)
+
+        buttonDelete.addEventListener('click', function() {
+            item = this.parentNode
+            item.remove()
+        })
+
+        return newListElement
+    }
+
+    
+    function getTODOListFromBackend() {
+        $.get('https://jsonplaceholder.typicode.com/todos', function(data, status){
+            const response = data
+            for(i=0; i < response.length; i++) {
+                list.append(createTODODynamically(response[i].id, response[i].title))
+            }
+        })
+        // const http = new XMLHttpRequest()
+        // http.onreadystatechange = function() {
+        //     if(this.readyState === 4 ){
+        //         if(this.status === 200) {
+        //             const response = JSON.parse(this.responseText)
+        //             for(i=0; i < response.length; i++) {
+        //                 list.append(createTODODynamically(response[i].id, response[i].title))
+        //             }
+        //         } else {
+        //             console.log("Called Failed")
+        //         }
+                
+        //     }
+        // }
+        // http.open('GET', 'https://jsonplaceholder.typicode.com/todos', true)
+        // http.send()
+    }
+
+    getTODOListFromBackend()
+
+
+    function createTODOItemAtBackend() {
+        // const http = new XMLHttpRequest()
+        // http.open('POST', 'https://jsonplaceholder.typicode.com/todos', true)
+        // http.onreadystatechange = function(){
+        //     if(this.readyState === 4) {
+        //         if(this.status === 201) {
+        //             const response = JSON.parse(this.responseText)
+        //             list.append(createTODODynamically(response.id, currentTodoInput))
+
+        //             // Clear Input
+        //             clearInputData()
+        //         } else {
+        //             console.log('Submission Failed')
+        //         }
+        //     }
+            
+        // }
+        // http.send(obj)
+
+        const obj = {
+            "userId": 1,
+            "title": currentTodoInput,
+            "completed": false
+        }
+        
+        $.post('https://jsonplaceholder.typicode.com/todos', obj, function(data, status) {
+            const response = data
+            list.append(createTODODynamically(response.id, currentTodoInput))
+
+            // Clear Input
+            clearInputData()
+        })
+    }
 })
 
